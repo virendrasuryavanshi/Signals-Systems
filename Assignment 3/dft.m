a662c00b3004c6bdf76b66fa0195775dd3bbf0cd
Suryavanshi Virendrasingh <mandib16037@gmail.com>
2018-05-27 13:39:28 +0530

    Update dft.m

--- Assignment 3/dft.m
+++ Assignment 3/dft.m
@@ -1,2 +1,43 @@
-x = [1 2 3 1 1 2 3 2 1 2 2]
-X = DFT_nik(x)
+function [X]=DFT_nik(x)
+
+N=length(x);
+for k=1:N
+    X(k)=0;
+    for n=1:N
+        X(k)=X(k)+x(n).*exp(-1j.*2.*pi.*(n-1).*(k-1)./N);
+    end
+end
+
+f=0:N-1;
+subplot(311)
+stem(0:N-1,x)
+title('Sequence x (in time domain)')
+xlabel('time')
+ylabel('Amplitude')
+grid;
+
+subplot(323)
+stem(f,abs(X))
+title('Magnitude of Fourier Coeffients using function')
+ylabel('|X|')
+grid;
+
+subplot(325)
+stem(f,angle(X))
+title('Angle of Fourier Coeffients using function')
+xlabel('Frquency coefficients')
+ylabel('<X')
+grid;
+
+subplot(324)
+stem(f,abs(fft(x)))
+title('Magnitude of Fourier Coeffients using fft function')
+ylabel('|X|')
+grid;
+
+subplot(326)
+stem(f,angle(fft(x)))
+title('Angle of Fourier Coeffients using fft function')
+xlabel('Frquency coefficients')
+ylabel('<X')
+grid;
